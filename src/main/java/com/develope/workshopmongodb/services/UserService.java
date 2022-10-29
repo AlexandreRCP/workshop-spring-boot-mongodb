a4980698531a19acd13c740067fc40194a930200
AlexandreRCP <alex.rcp@hotmail.com>
2022-10-29 16:56:56 -0300

    User Insertion with POST and User Deletion with DELETE

--- src/main/java/com/develope/workshopmongodb/services/UserService.java
+++ src/main/java/com/develope/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.develope.workshopmongodb.domian.User;
+import com.develope.workshopmongodb.dto.UserDTO;
 import com.develope.workshopmongodb.repository.UserRepository;
 import com.develope.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -22,6 +23,19 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);		
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
