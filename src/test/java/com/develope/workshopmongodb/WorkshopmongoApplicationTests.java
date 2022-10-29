f426fa339c85c41e16ea19fdc4ab62e6c250efe3
AlexandreRCP <alex.rcp@hotmail.com>
2022-10-29 13:08:50 -0300

    First commit - project created

---
+++ src/test/java/com/develope/workshopmongodb/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develope.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
