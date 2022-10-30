b9eb639ba97a3078229d2ad1017a6b072f4318a8
AlexandreRCP <alex.rcp@hotmail.com>
2022-10-30 15:27:42 -0300

    Adding comments to posts and Simple query with query methods

---
+++ src/main/java/com/develope/workshopmongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.develope.workshopmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.develope.workshopmongodb.domian.Post;
+import com.develope.workshopmongodb.repository.PostRepository;
+import com.develope.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+}
